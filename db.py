4bc35aa1ee65f5ce9360a9678d580e82fd1ae287
Shreya LR <shravyashreya26@gmail.com>
2026-02-06 15:17:26 +0530

    do

--- db.py
+++ db.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-DATABASE_URL = "./test.db"
+
+DATABASE_URL = "sqlite:///./test.db"
+
 def get_db():
-    db=create_engine(DATABASE_URL)
+    db = create_engine(DATABASE_URL)
     try:
         yield db
     finally:
