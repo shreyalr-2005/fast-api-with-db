2e2c49717630c41df74f06d5ad527a24b8462c91
Shreya LR <shravyashreya26@gmail.com>
2026-02-07 12:12:50 +0530

    do

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 print("DATABASE_URL",DATABASE_URL)
-engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 def get_db():
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from db import Base
 from sqlalchemy import Column, Integer, String
-class user(Base):
+class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
