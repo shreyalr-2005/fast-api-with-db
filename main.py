a7838acf7913e262f65ab4aa1c03f7289c90e927
Shreya LR <shravyashreya26@gmail.com>
2026-02-09 12:33:31 +0530

    do

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from fastapi import FastAPI
 from routes.user_routes import router as user_router
-from db import get_db, DATABASE_URL
+from routes.ai_response_routes import router as ai_response_router
+from db import get_db,DATABASE_URL
 from sqlalchemy import create_engine
 import os
 from models import Base
+
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(ai_response_router)
 #to create database
-engine = create_engine(DATABASE_URL)
+
+engine=create_engine(DATABASE_URL)
 Base.metadata.create_all(engine)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+
 
 if __name__ == "__main__":
     import uvicorn
---
+++ schemas/ai_response_schema.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class AIRequest(BaseModel):
+    message: str
+    system_prompt: str = "You are a helpful assistant."
+
+class AIResponse(BaseModel):
+    response: str
